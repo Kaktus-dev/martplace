7ad9b1a17d9eb634e2b79b1edbe06b67c485c8d4
Kaktus-dev <seintraub@gmail.com>
2020-11-29 23:13:46 +0300

    in the development release

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
-$(function(){
+$(function () {
     $(".feature__box-inners").slick({});
 
     var mixer = mixitup('.release__inner-box');
+
+    $(".feed__inner").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
 
 });
